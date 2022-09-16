20ce45effd0fc486752d804edb6cda6f8cfd80c8
dimsssss <caruen@hanmail.net>
2022-09-17 00:15:17 +0900

    feat: 라우터 등록

--- boss/index.js
+++ boss/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const bossController = require('./bossController')
+const bossController = require('./presentation/bossController')
+const {postValidator, patchValidator} = require('./presentation/bossValidator')
 
 router.get('/', bossController.getBossState)
+router.post('/', postValidator, bossController.enterBossRaid)
+router.patch('/', patchValidator, bossController.finishBossRaid)
 
 module.exports = router
