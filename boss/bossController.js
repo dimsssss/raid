12b491f8df0d05a8b93858f7307dd8c263d2378f
dimsssss <caruen@hanmail.net>
2022-09-16 22:21:18 +0900

    feat: 보스 레이드 시작 컨트롤러

--- boss/bossController.js
+++ boss/bossController.js
@@ -13,6 +13,23 @@ const getBossState = (req, res) => {
   }
 }
 
+const enterBossRaid = (validator, req, res) => {
+  try {
+    if (validator.error) {
+      return res.status(StatusCodes.BAD_REQUEST).send(validator.error.message)
+    }
+
+    const {bossStateCache} = req.app.get('bossStateCache')
+    const result = bossService.getBossState(bossStateCache)
+    return res.status(StatusCodes.OK).send(result)
+  } catch (err) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
+  }
+}
+
 module.exports = {
   getBossState,
+  enterBossRaid,
 }
