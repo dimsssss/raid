bd690f52b1ef69f93866a8575977b859548d1cb5
dimsssss <caruen@hanmail.net>
2022-09-16 19:14:59 +0900

    feat: boss state router 연결

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const userRouter = require('./user/index')
+const bossRouter = require('./boss/index')
 
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
@@ -13,11 +14,14 @@ const swaggerDocument = YAML.load('./swagger.yaml')
 
 const app = express()
 
+app.set('bossStateCache', {})
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(logger('dev'))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/user', userRouter)
+app.use('/bossRaid', bossRouter)
 
 module.exports = app
