835bbe9560efc6bee82d9422dce6a701a3fd4c20
dimsssss <caruen@hanmail.net>
2022-09-16 14:17:30 +0900

    test: 유저 생성 테스트

---
+++ __tests__/helper/user.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const db = require('../../bin/database')
+const {faker} = require('@faker-js/faker')
+
+const createUserData = () => {
+  const userDummy = []
+  for (let i = 0; i < 10; i += 1) {
+    const userName = faker.name.fullName()
+    userDummy.push({userName})
+  }
+  return userDummy
+}
+const createDefaultData = async () => {
+  try {
+    const {users} = db
+    const userData = createUserData()
+    const result = await users.bulkCreate(userData, {raw: true})
+    return result
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+const cleanDatabase = async () => {
+  try {
+    const {users} = db
+    await users.destroy({where: {}, force: true})
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+module.exports = {
+  createDefaultData,
+  cleanDatabase,
+}
