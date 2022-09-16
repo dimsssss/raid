5496e53832e96e24ae46a32ff4e32b122e6c7efb
dimsssss <caruen@hanmail.net>
2022-09-16 23:05:41 +0900

    chore: 계층 분리

--- boss/presentation/bossController.js
+++ boss/presentation/bossController.js
@@ -29,7 +29,24 @@ const enterBossRaid = (validator, req, res) => {
   }
 }
 
+const finishBossRaid = (validator, req, res) => {
+  try {
+    if (validator.error) {
+      return res.status(StatusCodes.BAD_REQUEST).send(validator.error.message)
+    }
+
+    const {bossStateCache} = req.app.get('bossStateCache')
+    const result = bossService.getBossState(bossStateCache)
+    return res.status(StatusCodes.OK).send(result)
+  } catch (err) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
+  }
+}
+
 module.exports = {
   getBossState,
   enterBossRaid,
+  finishBossRaid,
 }
