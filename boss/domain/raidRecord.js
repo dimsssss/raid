d99973a4cd7d5be1ad48d71662f6c96a8bb29253
dimsssss <caruen@hanmail.net>
2022-09-16 19:06:03 +0900

    feat: boss rade 내역 모델링

---
+++ boss/domain/raidRecord.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) => {
+  const raidRecord = sequelize.define(
+    'raidRecord',
+    {
+      raidRecordId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.ENUM,
+        values: ['enter', 'exit'],
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      timestamp: false,
+      paronid: true,
+    },
+  )
+
+  return raidRecord
+}
