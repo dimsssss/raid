cfc27e6482e095a0441f6d3aa91f5accea5b90b7
dimsssss <caruen@hanmail.net>
2022-09-17 00:04:03 +0900

    test: 레이즈 종료 테스트 헬퍼 코드

--- __tests__/helper/boss.js
+++ __tests__/helper/boss.js
@@ -1,6 +1,35 @@
 require('dotenv').config()
 
 const db = require('../../bin/database')
+const createRaidRecord = () => {
+  return [
+    {
+      raidRecordId: 1,
+      userId: 1,
+      state: 'start',
+      score: 50,
+      createdAt: new Date('2022-09-16 18:00').getDate(),
+      updatedAt: new Date('2022-09-16 18:00').getDate(),
+    },
+    {
+      raidRecordId: 2,
+      userId: 2,
+      state: 'start',
+      score: 50,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  ]
+}
+const initData = async () => {
+  try {
+    const {raidRecords} = db
+    const data = createRaidRecord()
+    return await raidRecords.bulkCreate(data, {raw: true})
+  } catch (err) {
+    throw new Error(err)
+  }
+}
 
 const cleanDatabase = async () => {
   try {
@@ -12,5 +41,6 @@ const cleanDatabase = async () => {
 }
 
 module.exports = {
+  initData,
   cleanDatabase,
 }
