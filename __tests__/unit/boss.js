4c4953aecb509facddc0c76a6565064f6c67d6be
dimsssss <caruen@hanmail.net>
2022-09-16 19:06:39 +0900

    test: boss 상태 조회 테스트

---
+++ __tests__/unit/boss.js
@@ -0,0 +1,39 @@
+/* global describe test expect */
+describe('boss raid 확인 테스트', () => {
+  test.each([
+    [{}, {canEnter: true}],
+    [
+      {
+        data: {
+          userId: 1,
+          createdAt: new Date('2022-09-16 18:02').getDate(),
+          state: 'start',
+        },
+      },
+      {canEnter: true},
+    ],
+    [
+      {
+        data: {
+          userId: 1,
+          createdAt: Date.now(),
+          state: 'start',
+        },
+      },
+      {canEnter: false, userId: 1},
+    ],
+    [
+      {
+        data: {
+          userId: 1,
+          createdAt: Date.now(),
+          state: 'end',
+        },
+      },
+      {canEnter: true},
+    ],
+  ])('입력 %p 결과 %p', (stateCache, expected) => {
+    const validateBossRaid = require('../../boss/dto/bossState')
+    expect(validateBossRaid.getBossState(stateCache)).toEqual(expected)
+  })
+})
