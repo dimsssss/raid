b48f4eb6ae21dbc5839f620d4c9e8a5970806d31
dimsssss <caruen@hanmail.net>
2022-09-16 11:52:08 +0900

    chore: 변수명 수정

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = {
   username: process.env.DATABASE_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
   host: process.env.HOST,
   port: process.env.DATABASE_PORT,
