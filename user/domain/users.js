e6fa8f37bc8fc13c12f48c5dcc8ffae7d2042c24
dimsssss <caruen@hanmail.net>
2022-09-16 13:39:04 +0900

    chore: 디렉터리명 수정

--- user/domain/users.js
+++ user/domain/users.js
@@ -1,49 +1,38 @@
-const {v4: uuidv4} = require('uuid')
-
 module.exports = (sequelize, DataTypes) => {
   const points = sequelize.define(
-    'sample',
+    'users',
     {
-      sampleId: {
+      userId: {
         type: DataTypes.UUID,
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4,
         allowNull: false,
       },
-      placeId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      reviewId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      hasBonus: {
-        type: DataTypes.TINYINT,
-        allowNull: false,
-      },
-      point: {
-        type: DataTypes.INTEGER,
+      userName: {
+        type: DataTypes.STRING,
         allowNull: false,
       },
       createdAt: {
         type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
         allowNull: false,
       },
       updatedAt: {
         type: DataTypes.DATE,
         allowNull: false,
         onUpdate: DataTypes.NOW,
+        defaultValue: DataTypes.NOW,
+      },
+      DELETEDaT: {
+        type: DataTypes.DATE,
+        allowNull: true,
       },
     },
     {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci',
-      freezeTableName: true,
+      timestamp: false,
+      paronid: true,
     },
   )
 
