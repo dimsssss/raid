c9fc8eb9b03189a92c8ebc6f26fd9ce09328a0dc
dimsssss <caruen@hanmail.net>
2022-09-17 19:07:12 +0900

    feat: 랭킹 조회 컨트롤러

--- boss/presentation/bossController.js
+++ boss/presentation/bossController.js
@@ -45,8 +45,24 @@ const finishBossRaid = (validator, req, res) => {
   }
 }
 
+const getRankings = (validator, req, res) => {
+  try {
+    if (validator.error) {
+      return res.status(StatusCodes.BAD_REQUEST).send(validator.error.message)
+    }
+
+    const result = bossService.getRankers(validator.value)
+    return res.status(StatusCodes.OK).send(result)
+  } catch (err) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
+  }
+}
+
 module.exports = {
   getBossState,
   enterBossRaid,
   finishBossRaid,
+  getRankings,
 }
