d02546b0c0322ff55104fd0689217bb071b9eb49
dimsssss <caruen@hanmail.net>
2022-09-19 00:45:46 +0900

    feat: 랭킹 조회 requestt validator 추가

--- boss/index.js
+++ boss/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const bossController = require('./presentation/bossController')
-const {postValidator, patchValidator} = require('./presentation/bossValidator')
+const {
+  postValidator,
+  patchValidator,
+  getValidator,
+} = require('./presentation/bossValidator')
 
-router.get('/topRankerList', bossController.getRankings)
+router.get('/topRankerList', getValidator, bossController.getRankings)
+
 router.post('/enter', postValidator, bossController.enterBossRaid)
 router.patch('/end', patchValidator, bossController.finishBossRaid)
 router.get('/', bossController.getBossState)
