726da21583abe2b3695990c6424c9ba84c7eecf0
dimsssss <caruen@hanmail.net>
2022-09-19 00:47:29 +0900

    feat: request validator 추가

--- boss/presentation/bossValidator.js
+++ boss/presentation/bossValidator.js
@@ -1,5 +1,9 @@
 const Joi = require('joi')
 
+const getSchema = Joi.object({
+  userId: Joi.number().min(1),
+})
+
 const postSchema = Joi.object({
   userId: Joi.number().min(1),
   level: Joi.number().min(1),
@@ -16,11 +20,17 @@ const patchSchema = Joi.object({
 })
 
 const patchValidator = (req, res, next) => {
-  const result = patchSchema.validate(req.query)
+  const result = patchSchema.validate(req.body)
+  next(result, req, res, next)
+}
+
+const getValidator = (req, res, next) => {
+  const result = getSchema.validate(req.body)
   next(result, req, res, next)
 }
 
 module.exports = {
   postValidator,
   patchValidator,
+  getValidator,
 }
