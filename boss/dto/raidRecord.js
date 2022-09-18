cc7d632febe4f2cde2eeb6bffaea310e5b8cde94
dimsssss <caruen@hanmail.net>
2022-09-18 22:12:35 +0900

    feat: 보스 상태 값 반환 메서드 두 개로 나눔

--- boss/dto/raidRecord.js
+++ boss/dto/raidRecord.js
@@ -1,28 +1,3 @@
-/**
- * 보스 레이드가 처음이거나, 시작하고 3분이 지났거나, 마지막 상태가 끝났을 경우 false
- * 그 외에 true
- * @param {*} record
- * @returns boolean
- */
-const isRaiding = record => {
-  if (record === undefined) {
-    return false
-  }
-
-  if (record.state === 'end') {
-    return false
-  }
-
-  const raidStartDate = new Date(record.createdAt)
-  raidStartDate.setMinutes(raidStartDate.getMinutes() + 3)
-
-  if (record.state === 'start' && Date.now() >= raidStartDate) {
-    return false
-  }
-
-  return true
-}
-
 const getScoreFor = (bossRaidInformation, level) => {
   const levelInformation = bossRaidInformation.levels.filter(information => {
     return level === information.level
@@ -55,8 +30,18 @@ const getBossState = cache => {
       userId: cache.data.userId,
     }
   }
+}
+
+const getPossibleRaidState = () => {
   return {
     canEnter: true,
+  }
+}
+
+const getImpossibleRaidState = cache => {
+  return {
+    canEnter: false,
+    userId: cache.data.userId,
   }
 }
 
@@ -87,7 +72,8 @@ const splitToUserRankingAndAllRanking = (records, userId) => {
 
 module.exports = {
   getBossState,
-  isRaiding,
+  getPossibleRaidState,
+  getImpossibleRaidState,
   crateNewRaidBossRecord,
   setBossStateCache,
   getStartBossRaidInformation,
