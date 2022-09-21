6d83c0a6953ab46597f283de737a11adb4f98a0e
dimsssss <caruen@hanmail.net>
2022-09-21 11:42:08 +0900

    feat: default 값 설정

--- boss/domain/bossState.js
+++ boss/domain/bossState.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       state: {
         type: DataTypes.ENUM,
         values: ['open', 'close'],
+        defaultValue: 'open',
         allowNull: false,
       },
       createdAt: {
