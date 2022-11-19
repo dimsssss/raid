91815a450a5f30508369c93f327b30b2d702f342
dimsssss <caruen@hanmail.net>
2022-11-19 16:38:50 +0900

    feat: 서버 시작 스크립트 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 echo "module init"
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
-nvm install --lts
+nvm install 16.18.1
+
+node -e "console.log('Running Node.js ' + process.version)"
+
+npm install
+
+pm2 kill
+
+pm2 start ./bin/index
