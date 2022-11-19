9c637c514abc2703284fa5837ea80918bc5c9f27
dimsssss <caruen@hanmail.net>
2022-11-19 17:43:05 +0900

    fix: 스크립트 분리

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "module init"
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install 16.18.1
+
+node -e "console.log('Running Node.js ' + process.version)"
+
+cd ~/app
+
+npm install
