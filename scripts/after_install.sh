bccb864f883d79e6f6e635dcb14d49165a7f72c0
dimsssss <caruen@hanmail.net>
2022-11-19 18:11:36 +0900

    feat:  배포 script 수정

---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /home/ec2-user/app
+
+cd /home/ec2-user/app
+
+npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,9 +4,8 @@ echo "module init"
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
 sudo yum install -y nodejs npm
+sudo yum install npm  -y
+sudo npm install pm2 -g
 
 node -e "console.log('Running Node.js ' + process.version)"
 
-cd ~/app
-
-npm install
